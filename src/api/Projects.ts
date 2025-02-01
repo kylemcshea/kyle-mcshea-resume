10146fb6631961a0bbd09b66cde08f311aedfe59
Kyle McShea <46171436+kylemcshea@users.noreply.github.com>
2025-02-01 01:11:14 -0500

    chore: code cleanup

--- src/api/Projects.ts
+++ src/api/Projects.ts
@@ -1,6 +1,5 @@
 import Breez from "../assets/breez_logo.png";
 import DreamScripts from "../assets/dream_scripts_logo.png";
-import VerseLabs from "../assets/VerseLabs_Simple_Background.png";
 
 export interface ProjectProps {
   website_url: string;
@@ -11,18 +10,11 @@ export interface ProjectProps {
 
 export const Projects: ProjectProps[] = [
   {
-    website_url: "https://verselabs.io/",
-    name: "Verse Labs",
-    image_src: VerseLabs,
-    description:
-      "Worked with over 5 clients in the North East Region. We build web and mobile applications.",
-  },
-  {
-    website_url: "https://discord.gg/KysbXbrYNN",
-    name: "DreamScripts",
+    website_url: "https://github.com/kylemcshea/dream-postal",
+    name: "GTA Modding",
     image_src: DreamScripts,
     description:
-      "2,000+ Users. Online Ecommerce store to sell Grand Theft Auto v Modification Scripts. React, TypeScript, Lua, MySQL",
+      "Created a fairly popular Grand Theft Auto 5 Modification Scripts. Built this using Lua!",
   },
   {
     website_url: "https://breez-music-kylemcshea.vercel.app/",
